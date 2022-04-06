a2200ac94ad91692670b067696f9cba116bb4c9d
Steve Morgan <59463932+Morgan237@users.noreply.github.com>
2022-04-07 00:23:12 +0100

    Update TextEditor.java

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -294,25 +294,21 @@ public void windowClosing(WindowEvent arg0) {
 
 @Override
 public void windowDeactivated(WindowEvent arg0) {
-	// TODO Auto-generated method stub
 	
 }
 
 @Override
 public void windowDeiconified(WindowEvent arg0) {
-	// TODO Auto-generated method stub
 	
 }
 
 @Override
 public void windowIconified(WindowEvent arg0) {
-	// TODO Auto-generated method stub
 	
 }
 
 @Override
 public void windowOpened(WindowEvent arg0) {
-	// TODO Auto-generated method stub
 	textArea.requestFocus();
 }
 
